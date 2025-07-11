03552c739a09f847427e7b80efeb833214658f6d
Vivekwaghm <waghmarevivek13@gmail.com>
2025-07-11 22:32:17 +0530

    Movie app android first commit

---
+++ app/(tabs)/search.jsx
@@ -0,0 +1,40 @@
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
+import useFetch from '@/services/useFetch'
+import { useRouter } from 'expo-router'
+import { fetchMovies } from '@/services/api'
+
+const search = () => {
+ const router = useRouter()
+  const {
+      data : movies,
+      loading: moviesLoading,
+      error: moviesError
+    } = useFetch(() => fetchMovies({
+    query: ''
+  }))
+  return (
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg}  className='flex-1 absolute w-full z-0' resizeMode='cover'/>
+      <FlatList data={movies} renderItem={({item}) => 
+        <MovieCard {...item}/>
+      }
+      keyExtractor={(item) => item.id.toString()}
+      className='px-5'
+      numColumns={3}
+      columnWrapperStyle={{
+        justifyContent:'center',
+        gap:16,
+        marginVertical:16
+      }}
+      contentContainerStyle={{ paddingBottom: 100}}
+      />
+    </View>
+  )
+}
+
+export default search
+
+const styles = StyleSheet.create({})
